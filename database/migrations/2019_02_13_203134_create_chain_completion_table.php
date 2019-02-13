f444aaac66f775e7d604e9ddb7fd6399a4e57cca
buzz1274 <david@sulaco.co.uk>
2019-02-13 21:00:13 +0000

    db structure

---
+++ database/migrations/2019_02_13_203134_create_chain_completion_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChainCompletionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chain_completion', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('chain_id')->unsigned();
+            $table->foreign('chain_id')->references('id')->on('chain');
+            $table->date('completion_date');
+            $table->boolean('completed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chain_completion');
+    }
+}
