4780e90f9a1d116aa22ebcb28b498dbaaff3cc3c
buzz1274 <david@sulaco.co.uk>
2019-07-16 12:03:03 +0100

    format outstanding completed date

--- resources/assets/js/templates/pages/outstanding.jsx
+++ resources/assets/js/templates/pages/outstanding.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import MainInterface from '../components/main_interface.jsx';
 import '../../../css/pages/outstanding.css';
 
@@ -10,12 +11,41 @@ export default class Outstanding extends React.Component {
         updateState: PropTypes.func.isRequired
     }
 
+    today = false;
+
     constructor(props) {
         super(props);
     }
 
     componentDidMount() {
+        this.today = moment().format('Y-MM-DD');
         this.props.chains.getOutstanding();
+    }
+
+    formatOutstandingChain(outstanding) {
+        let output = outstanding.chain;
+
+        if(outstanding.frequency == 'daily' || outstanding.frequency == 'weekday') {
+            if(this.today == outstanding.chain_completion_date) {
+                output += ' today';
+            } else {
+                output += ' on ' + moment(outstanding.chain_completion_date).format('Do, MMMM Y');
+            }
+        } else if(outstanding.frequency == 'weekly') {
+            if(this.today == outstanding.chain_completion_date) {
+                output += ' this week';
+            } else {
+                output += ' week ending on ' + moment(outstanding.chain_completion_date).format('Do, MMMM Y');
+            }
+        } else if(outstanding.frequency == 'weekly') {
+            if (this.today == outstanding.chain_completion_date) {
+                output += ' this week';
+            } else {
+                output += ' in ' + moment(outstanding.chain_completion_date).format('MMMM Y');
+            }
+        }
+
+        return output;
     }
 
     displayOutstanding(outstanding) {
@@ -23,7 +53,7 @@ export default class Outstanding extends React.Component {
             <div className='container d-flex outstanding-outstanding' key={outstanding.chain_completion_id}>
                 <div className='outstanding-outstanding-content row col-12 align-self-center'>
                     <div className='row col-9 outstanding-outstanding-content-chain'>
-                        {outstanding.chain} on {outstanding.chain_completion_date}
+                        {this.formatOutstandingChain(outstanding)}
                     </div>
                     <div className='outstanding-outstanding-buttons row col-4 justify-content-end'>
                         <button type='button' className='btn btn-success'
