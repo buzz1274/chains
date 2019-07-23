4eb10acbca843eba2c2533c9bdc3219fb77e5002
buzz1274 <david@sulaco.co.uk>
2019-07-23 19:19:39 +0100

    Email class & Refactoring

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,10 +7,37 @@ use App\User;
 
 class UserController extends Controller
 {
+    private $user = false;
+
+    public function __construct()
+    {
+        $this->user = new User();
+    }
+
+    /**
+     * Register a new user
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function register(Request $request)
     {
-        $errors = false;
+        $result = $this->user->add(
+            [
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => $request->input('password'),
+            ]
+        );
 
+        if (is_array($result)) {
+            return response()->json($result, 400);
+        } elseif ($result === true) {
+            return response()->json(['Registration successful'], 201);
+        }
+
+        return response()->json(['Registration failed'], 500);
+
+        /*
         foreach (['name', 'email', 'password', 'repeatPassword'] as $field) {
             if (empty($request->input($field))) {
                 $errors[$field] = 'Please enter a value';
@@ -38,17 +65,6 @@ class UserController extends Controller
                 $errors['repeatPassword'] = $errors['password'];
             }
         }
-
-        if ($errors) {
-            return response()->json($errors, 400);
-        }
-
-        User::add(
-            $request->input('name'),
-            $request->input('email'),
-            $request->input('password')
-        );
-
-        return response()->json(['Registration successful'], 201);
+        */
     }
 }
--- app/User.php
+++ app/User.php
@@ -5,10 +5,18 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Support\Facades\Hash;
+use App\Helper\Email;
 
 class User extends Authenticatable
 {
     use Notifiable;
+
+    /**
+     * instance of email class
+     *
+     * @var object
+     */
+    private $email = false;
 
     /**
      * The attributes that are mass assignable.
@@ -28,27 +36,111 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public static function add($name, $email, $password)
+    /**
+     * User constructor.
+     */
+    public function __construct()
     {
-        self::insert(
-            [
-                'name' => $name,
-                'email' => $email,
-                'password' => Hash::make($password)
-            ]
-        );
-
-        //send registration email//
+        $this->email = new Email();
     }
 
-    public static function emailAlreadyInUse($email)
+    /**
+     * @param $registrationDetails
+     *              - name      string
+     *              - email     string
+     *              - password  string
+     * @return bool|mixed
+     */
+    public function add($registrationDetails)
+    {
+        try {
+            $errors = $this->validRegistrationDetails($registrationDetails);
+
+            if (is_array($errors)) {
+                return $errors;
+            }
+
+            $this->insert(
+                [
+                    'name' => $registrationDetails['name'],
+                    'email' => $registrationDetails['email'],
+                    'password' => Hash::make($registrationDetails['password'])
+                ]
+            );
+
+            $this->sendRegistrationEmail($registrationDetails['email'], []);
+
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+    private function validRegistrationDetails($registrationDetails)
+    {
+        $errors = [];
+
+        foreach (['name', 'email', 'password', 'repeatPassword'] as $field) {
+            if (empty($registrationDetails[$field])) {
+                $errors[$field] = 'Please enter a value';
+            }
+        }
+
+        if (!isset($errors['email']) &&
+            $this->emailAlreadyInUse($registrationDetails['email'])) {
+            $errors['email'] = 'Email address already in use';
+        }
+
+        if (!isset($errors['password']) && !isset($errors['repeatPassword'])) {
+            if ($registrationDetails['password'] !=
+                $registrationDetails['repeatPassword']) {
+                $errors['password'] = 'Passwords do not match';
+                $errors['repeatPassword'] = $errors['password'];
+            }
+
+            if (!isset($errors['password']) &&
+                !$this->passwordMatchesPolicy(
+                    $registrationDetails['password']
+                )) {
+                $errors['password'] =
+                    'Please enter a password of at least 7 characters with at '.
+                    'least 1 number, 1 uppercase character and '.
+                    '1 punctuation character';
+                $errors['repeatPassword'] = $errors['password'];
+            }
+        }
+
+        if (!empty($errors)) {
+            return $errors;
+        }
+
+        return true;
+    }
+
+    private function sendRegistrationEmail($email, $vars)
+    {
+        $this->email->send(
+            $email,
+            'chains.zz50.co.uk Registration',
+            'registration_email',
+            $vars
+        );
+    }
+
+
+
+
+
+
+
+    public function emailAlreadyInUse($email)
     {
         return (bool)self::select(['id'])->
                         where('email', '=', $email)->
                         count();
     }
 
-    public static function passwordMatchesPolicy($password)
+    public function passwordMatchesPolicy($password)
     {
         if (strlen($password) < 7) {
             return false;
