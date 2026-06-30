3d72bf02e7ce75b829c2123bf4ad66cbfc1f52a1
David <david@sulaco.co.uk>
2026-06-30 14:07:17 +0100

    remove debug code

--- backend/bin/start_container.sh
+++ backend/bin/start_container.sh
@@ -3,6 +3,5 @@
 if [ "$ENVIRONMENT" == "development" ] ; then
   uv run uvicorn app.main:app --host=0.0.0.0 --port=8002 --reload --no-access-log --log-level warning
 else
-  sleep infinity
-  #uv run uvicorn app.main:app --host=0.0.0.0 --port=8002 --no-access-log --log-level warning
+  uv run uvicorn app.main:app --host=0.0.0.0 --port=8002 --no-access-log --log-level warning
 fi
