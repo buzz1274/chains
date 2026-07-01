5e07592024d1b12797d5b238ab0ec7351e73f9e8
David <david@sulaco.co.uk>
2026-07-01 16:46:32 +0100

    backup database to S3

--- backend/app/scripts/__init__.py
+++ backend/app/scripts/__init__.py
@@ -1,6 +1,4 @@
 from app.core.config.config import settings
 from app.core.logging import configure_logging
-from structlog.contextvars import clear_contextvars
 
 configure_logging(settings.DEBUG)
-clear_contextvars()
--- backend/app/scripts/database/backup_db.py
+++ backend/app/scripts/database/backup_db.py
@@ -2,29 +2,30 @@ from typing import Optional
 
 import typer
 from botocore.exceptions import ClientError
-from app.core.config.config import settings
 import boto3
 from mypy_boto3_s3 import S3Client
 from datetime import datetime, UTC
 import os
 import subprocess  # nosec
 import tempfile
+
+from app.core.config.config import settings
 from app.core.config.settings import Settings
 import structlog
+from structlog.contextvars import clear_contextvars, bind_contextvars
 
 
 class BackupDB:
     NO_DUMP_CREATED_ERROR: str = "Failed to generate DB dump"
     FAILED_TO_RETRIEVE_ERROR: str = "Failed to retrieve old backups"
     FAILED_TO_DELETE_ERROR: str = "Failed to delete old backup"
-    FAILED_TO_CONNECT_ERROR: str = "Failed to connect to S3"
     BACKUPS_DISABLED_ERROR: str = "Backups are disabled in development mode"
     FAILED_TO_UPLOAD_ERROR: str = "Failed to upload DB dump to S3"
 
     def __init__(
         self,
         backup_settings: Optional[Settings] = None,
-        logger: Optional[structlog.BoundLogger] = None
+        logger: Optional[structlog.BoundLogger] = None,
     ):
         self.settings: Settings = backup_settings or settings
         self.logger: structlog.BoundLogger = logger or structlog.get_logger()
@@ -36,8 +37,6 @@ class BackupDB:
 
     def backup(self) -> None:
         """backup database to file and upload to S3"""
-        log: structlog.BoundLogger = self.logger
-
         try:
             with tempfile.TemporaryDirectory() as tmp_dir:
                 file_name: str = (
@@ -45,12 +44,13 @@ class BackupDB:
                 )
                 full_backup_path: str = os.path.join(tmp_dir, file_name)
 
-                log = log.bind(
+                bind_contextvars(
+                    script=__name__,
                     file_name=file_name,
-                    full_backup_path=full_backup_path
+                    full_backup_path=full_backup_path,
                 )
 
-                log.info("Starting database backup")
+                self.logger.info("Starting database backup")
 
                 self._backup_database(full_backup_path)
                 self.s3_client.upload_file(
@@ -58,12 +58,17 @@ class BackupDB:
                     self.settings.AWS_S3_BUCKET_NAME,
                     f"{self.settings.AWS_S3_BACKUP_PATH}{file_name}",
                 )
-                self._clean_old_backups()
+                self.logger.info("Backup uploaded to S3")
+                self._delete_old_backups()
 
-                log.info("Completed database backup")
-        except RuntimeError as e:
-            log.error(f"Failed database backup: {str(e)}", exc_info=True)
+                self.logger.info("Completed database backup")
+        except (RuntimeError, OSError) as e:
+            self.logger.error(
+                f"Failed database backup: {str(e)}", exc_info=True
+            )
             raise
+        finally:
+            clear_contextvars()
 
     def _backup_database(self, full_backup_path: str) -> None:
         """dump database to file"""
@@ -82,9 +87,13 @@ class BackupDB:
             except subprocess.CalledProcessError as e:
                 raise RuntimeError(self.NO_DUMP_CREATED_ERROR) from e
 
-            if (not os.path.isfile(full_backup_path) or
-                os.stat(full_backup_path).st_size == 0):
+            file_size = os.stat(full_backup_path).st_size
+
+            if file_size == 0:
                 raise RuntimeError(self.NO_DUMP_CREATED_ERROR)
+
+            bind_contextvars(file_size=file_size)
+            self.logger.info("Database backup created")
 
     def _db_backup_command(self) -> list[str]:
         return [
@@ -98,8 +107,8 @@ class BackupDB:
             self.settings.POSTGRES_DB,
         ]
 
-    def _clean_old_backups(self) -> None:
-        """clean old backups from S3"""
+    def _delete_old_backups(self) -> None:
+        """delete old backups from S3"""
         try:
             all_files = self.s3_client.list_objects_v2(
                 Bucket=self.settings.AWS_S3_BUCKET_NAME,
@@ -110,19 +119,21 @@ class BackupDB:
 
         try:
             valid_files = sorted(
-                (
-                    file
-                    for file in all_files if file["Key"].endswith(".sql")
-                ),
+                (file for file in all_files if file["Key"].endswith(".sql")),
                 key=lambda x: x["LastModified"],
                 reverse=True,
             )
 
-            for file in valid_files[self.settings.DAYS_BACKUPS_TO_KEEP:]:
+            files_to_delete = valid_files[self.settings.DAYS_BACKUPS_TO_KEEP :]
+
+            for file in files_to_delete:
                 self.s3_client.delete_object(
                     Bucket=self.settings.AWS_S3_BUCKET_NAME,
                     Key=file["Key"],
                 )
+
+            bind_contextvars(deleted_files=len(files_to_delete))
+            self.logger.info("Old backups deleted")
         except ClientError as e:
             raise RuntimeError(self.FAILED_TO_DELETE_ERROR) from e
 
@@ -130,8 +141,10 @@ class BackupDB:
         """initialize boto S3 client"""
         return boto3.client("s3")
 
+
 app = typer.Typer()
+
 
 @app.command()
 def backup_db():
-    BackupDB(settings, structlog.get_logger()).backup()
+    BackupDB().backup()
